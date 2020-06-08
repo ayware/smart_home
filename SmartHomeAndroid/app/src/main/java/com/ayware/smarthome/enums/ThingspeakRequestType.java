7faf256e84a0d78ee51f248c5c99813ae04a114d
Ayhan Yazici <yaziciayhann@gmail.com>
2020-06-08 19:12:57 +0300

    Some changes

--- SmartHomeAndroid/app/src/main/java/com/ayware/smarthome/enums/ThingspeakRequestType.java
+++
@@ -1,7 +0,0 @@
-package com.ayware.smarthome.enums;
-
-public enum ThingspeakRequestType {
-
-    GET_DATA,
-    POST_DATA
-}
--- SmartHomeAndroid/app/src/main/java/com/ayware/smarthome/listeners/OnAdapterListener.java
+++
@@ -1,9 +0,0 @@
-package com.ayware.smarthome.listeners;
-
-import com.ayware.smarthome.models.Item;
-
-public interface OnAdapterListener {
-
-    public void onDataChange(Item item);
-
-}
